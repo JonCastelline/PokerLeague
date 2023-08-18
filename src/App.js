f0c35f8b6f7b2d90cf2d4cd5c1d0c4fccfeae8c5
Jon Castelline <jon.castelline@gmail.com>
2023-08-18 11:26:47 -0500

    Allow for initial setup of players playing or not playing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -13,15 +13,15 @@ LogBox.ignoreLogs(['Remote debugger']);
 const Stack = createStackNavigator();
 
 export default function App() {
-
-    const players = [
-        { id: 1, firstName: "John", lastName: "Doe", kills: 0, place: 0, bounties: 0, points: 5, hasBounty: false },
-        { id: 2, firstName: "Jane", lastName: "Smith", kills: 0, place: 0, bounties: 0, points: 11, hasBounty: false },
-        { id: 3, firstName: "Mike", lastName: "Johnson", kills: 0, place: 0, bounties: 0, points: 3, hasBounty: false },
-        { id: 4, firstName: "Sarah", lastName: "Williams", kills: 0, place: 0, bounties: 0, points: 11, hasBounty: false },
-        { id: 5, firstName: "David", lastName: "Brown", kills: 0, place: 0, bounties: 0, points: 0, hasBounty: false },
-        { id: 6, firstName: "Emily", lastName: "Jones", kills: 0, place: 0, bounties: 0, points: 6, hasBounty: false },
-    ];
+    // Initialize players state using useState
+    const [players, setPlayers] = useState([
+        { id: 1, firstName: "John", lastName: "Doe", kills: 0, place: 0, bounties: 0, points: 5, hasBounty: false, playing: true },
+        { id: 2, firstName: "Jane", lastName: "Smith", kills: 0, place: 0, bounties: 0, points: 11, hasBounty: false, playing: true },
+        { id: 3, firstName: "Mike", lastName: "Johnson", kills: 0, place: 0, bounties: 0, points: 3, hasBounty: false, playing: true },
+        { id: 4, firstName: "Sarah", lastName: "Williams", kills: 0, place: 0, bounties: 0, points: 11, hasBounty: false, playing: true },
+        { id: 5, firstName: "David", lastName: "Brown", kills: 0, place: 0, bounties: 0, points: 0, hasBounty: false, playing: true },
+        { id: 6, firstName: "Emily", lastName: "Jones", kills: 0, place: 0, bounties: 0, points: 6, hasBounty: false, playing: true },
+    ]);
 
     // Calculate the player with the highest points and set hasBounty property
     const maxPoints = Math.max(...players.map(player => player.points));
@@ -30,6 +30,10 @@ export default function App() {
       hasBounty: player.points === maxPoints,
     }));
 
+    const updatePlayers = (updatedPlayers) => {
+        setPlayers(updatedPlayers);
+    };
+
     return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login">
@@ -37,7 +41,7 @@ export default function App() {
           <Stack.Screen name="Home" component={HomePage} />
           <Stack.Screen name="Standings" component={StandingsPage} />
           <Stack.Screen name="Play">
-            {props => <PlayPage {...props} players={playersWithBounties} />}
+            {props => <PlayPage {...props} players={playersWithBounties} updatePlayers={updatePlayers}/>}
           </Stack.Screen>
         </Stack.Navigator>
       </NavigationContainer>
